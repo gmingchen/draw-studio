897a34021930de6204d4d667f4f0c01d9ce67b96
gumingchen <1240235512@qq.com>
2026-01-24 17:22:29 +0800

    初始化项目

---
+++ play/vue3/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    {{ color }}
+    <DrawBoard
+      v-model:color="color"
+      width="500"
+      height="500"
+      background-color="#FFFFFF" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import DrawBoard from '@draw-board/draw-board-for-vue3'
+
+const color = ref('red')
+
+</script>
+
+<style scoped>
+.app {
+  padding: 20px;
+  width: 700px;
+  height: 700px;
+  margin: 0 auto;
+  background-color: #A3A0A0FF;
+}
+</style>
